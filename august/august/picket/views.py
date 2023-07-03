9f9fcc7a6a732beb5096a68a1e7e2aee46b8c89f
anildelikaya <anilberkdelikaya@gmail.com>
2023-07-03 18:17:44 +0300

    views,urls, picket api and signup urls

---
+++ august/august/picket/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from datetime import datetime
+
+
+# Accept a request, return a string
+def home(request):
+    return HttpResponse("API Service!, You're looking at Picket")
+
+def signup(request):
+    return HttpResponse(f"Account Registration: {datetime.now()}, You need a user name and passwoerd!")
